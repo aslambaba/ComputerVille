0c0bb9f940f74cf7ad67c5c138c8b51ca8aa6b59
Aslam Sarfraz <aslam91r@gmail.com>
2020-11-25 03:17:03 +0500

    Recieve Data from Stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ['Price'],
+        secretKey: 'sk_test_51Hr8RFEAxjBMcLOouNwcfCVPxQGNJJaEYmVXmHXR5DBNwLxkfaBAMiDdyk6uCrMIlVoOisjjtOGyaX4SiAEkMIoT00V67JiBu0',
+        downloadFiles: true,
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default function Products(props) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query ProductPrices {
+          prices: allStripePrice(
+            filter: { active: { eq: true } }
+            sort: { fields: [unit_amount] }
+          ) {
+            edges {
+              node {
+                id
+                active
+                currency
+                unit_amount
+                product {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={({ prices }) => (
+        <div>
+          {prices.edges.map(({ node: price }) => (
+            <p key={price.id}>{price.product.name}</p>
+          ))}
+        </div>
+      )}
+    />
+  )
+}
